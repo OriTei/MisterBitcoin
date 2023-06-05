6d6cf1a876344a7daaee13f6ef72fadc2b0aec35
Teicher Ori <teicheror@mta.ac.il>
2023-06-05 17:19:49 +0300

    hamburger menu added

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -1,22 +1,66 @@
-import React, { Component } from 'react'
-import logo from '../assets/imgs/svg.bit.png'
-import { Link } from 'react-router-dom'
+import React, { Component } from 'react';
+import logo from '../assets/imgs/svg.bit.png';
+import { Link } from 'react-router-dom';
+
 export default class AppHeader extends Component {
-    render() {
-        return (
-            <section className="header-container">
-                <nav>
-                    <Link to={'/'}>Home</Link>
-                    <Link to={'/about'}>About</Link>
-                    <Link to={'/contacts'}>Contacts</Link>
-                </nav>
-                <div className="logo-container">
-                    <Link to="/">
-                        <h1>MisterBitcoin</h1>
-                        <img src={logo} alt="bitcoin-logo" />
-                    </Link>
-                </div>
-            </section>
-        )
-    }
+  state = {
+    isMenuShown: false,
+    isMobile: window.innerWidth < 768
+  };
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleWindowResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleWindowResize);
+  }
+
+  handleWindowResize = () => {
+    this.setState({ isMobile: window.innerWidth < 768 });
+  };
+
+  toggleMenu = () => {
+    this.setState((prevState) => ({
+      isMenuShown: !prevState.isMenuShown
+    }));
+  };
+
+  render() {
+    const { isMenuShown, isMobile } = this.state;
+
+    return (
+      <section className={isMenuShown ? 'header-container blur' : 'header-container'}>
+        <nav>
+          {isMobile && (
+            <div className="hamburger-menu" onClick={this.toggleMenu}>
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <div className="bar"></div>
+              <nav className={isMenuShown ? 'side-menu open' : 'side-menu'}>
+                <Link to={'/'}>Home</Link>
+                <Link to={'/about'}>About</Link>
+                <Link to={'/contacts'}>Contacts</Link>
+              </nav>
+            </div>
+          )}
+          {!isMobile && (
+            <>
+              <Link to={'/'}>Home</Link>
+              <Link to={'/about'}>About</Link>
+              <Link to={'/contacts'}>Contacts</Link>
+            </>
+          )}
+        </nav>
+        <div className="logo-container">
+          <Link to="/">
+            <h1>MisterBitcoin</h1>
+            <img src={logo} alt="bitcoin-logo" />
+          </Link>
+        </div>
+
+        {/* Your main content goes here */}
+      </section>
+    );
+  }
 }
